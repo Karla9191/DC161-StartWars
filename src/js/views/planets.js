cd195d7552b8fd504a6552be08d7442f1bbd1fcd
Karla9191 <marietha2014@gmail.com>
2021-04-18 21:43:41 +0000

    final

--- src/js/views/planets.js
+++ src/js/views/planets.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from "react";
-import { Jumbotron, Button, Card } from "react-bootstrap";
+import { Jumbotron, Button, Card , CardDeck} from "react-bootstrap";
 import { Context } from "../store/appContext";
 
 export const Planets = () => {
@@ -10,35 +10,39 @@ export const Planets = () => {
 	}, []);
 
 	return (
+    <CardDeck>
 		<Card style={{ width: "18rem" }}>
 			<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
 			<Card.Body>
-				{store.planetsList.map((item, index) => {
-					return (
-						<li key={index}>
-							<Card.Title>
-								<h5>
-									<span>{item.name}</span>
-								</h5>
-							</Card.Title>
-							<Card.Text>
-								<h6>
-									Population: <span>{item.population}</span>
-								</h6>
-								<h6>
-									Terrain: <span>{item.terrain}</span>
-								</h6>
-								{store.favorites.includes(item.name) ? null : (
-									<Button onClick={() => actions.setFavorites(item.name)} variant="outline-primary">
-										Agregar
-									</Button>
-								)}
-							</Card.Text>
-						</li>
-					);
-				})}
+                 <ul> 
+                    {store.planetsList.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <Card.Title>
+                                    <h5>
+                                        <span>{item.name}</span>
+                                    </h5>
+                                </Card.Title>
+                                <Card.Text>
+                                    <h6>
+                                        Population: <span>{item.population}</span>
+                                    </h6>
+                                    <h6>
+                                        Terrain: <span>{item.terrain}</span>
+                                    </h6>
+                                    {store.favorites.includes(item.name) ? null : (
+                                        <Button onClick={() => actions.setFavorites(item.name)} variant="outline-primary">
+                                            Agregar
+                                        </Button>
+                                    )}
+                                </Card.Text>
+                            </li>
+                        );
+                    })}
+                </ul>
 			</Card.Body>
 		</Card>
+     </CardDeck>
 	);
 };
 
