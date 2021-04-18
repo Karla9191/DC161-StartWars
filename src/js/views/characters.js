064f40a7d77d5d2387cc176ff35b32c963768959
Karla9191 <marietha2014@gmail.com>
2021-04-18 20:58:46 +0000

    kmkmk

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -17,7 +17,15 @@ export const Characters = () => {
 				{store.peopleList.map((item, index) => {
 					return (
 						<li key={index}>
+							<h5>Name:</h5>
 							<span>{item.name}</span>
+							<h5>Height: </h5> <span>{item.height}</span>
+							<h5>Mass: </h5> <span>{item.mass}</span>
+							<h5>Hair color: </h5> <span>{item.hair_color}</span>
+							<h5>Skin color: </h5> <span>{item.skin_color}</span>
+							<h5>Eye color: </h5> <span>{item.eye_color}</span>
+							<h5>Birth year: </h5> <span>{item.birth_year}</span>
+							<h5>Gender: </h5> <span>{item.gender}</span>
 							{store.favorites.includes(item.name) ? null : (
 								<Button onClick={() => actions.setFavorites(item.name)} variant="outline-primary">
 									Agregar
