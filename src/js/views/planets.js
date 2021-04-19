4aefaeae1a59b19480627bc80f17c280bc60a22f
Karla9191 <marietha2014@gmail.com>
2021-04-19 02:32:10 +0000

    cambios de planetas

--- src/js/views/planets.js
+++ src/js/views/planets.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from "react";
-import { Jumbotron, Button, Card, CardDeck } from "react-bootstrap";
+import { Jumbotron, Button } from "react-bootstrap";
 import { Context } from "../store/appContext";
 import { BsHeart } from "react-icons/bs";
 
@@ -11,46 +11,34 @@ export const Planets = () => {
 	}, []);
 
 	return (
-		<CardDeck>
-			<Card style={{ width: "25rem" }}>
-				<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />{" "}
-				<Card.Body>
-					<ul>
-						{store.planetsList.map((item, index) => {
-							return (
-								<li key={index}>
-									{" "}
-									<Card.Title>
-										<h5>
-											<span>{item.name}</span>
-										</h5>
-									</Card.Title>{" "}
-									<Card.Text>
-										<h6>
-											Population: <span>{item.population}</span>
-										</h6>
-										<h6>
-											Terrain: <span>{item.terrain}</span>
-										</h6>
-										<Button variant="outline-primary">Learn more!</Button>{" "}
-										{store.favorites.includes(item.name) ? null : (
-											<Button
-												onClick={() => actions.setFavorites(item.name)}
-												variant="outline-warning">
-												<BsHeart />
-											</Button>
-										)}
-									</Card.Text>
-								</li>
-							);
-						})}
-					</ul>
-				</Card.Body>
-			</Card>
-		</CardDeck>
+		<Jumbotron>
+			<ul>
+				{store.planetsList.map((item, index) => {
+					return (
+						<li key={index} id="1">
+							{" "}
+							<h5>
+								<span>{item.name}</span>
+							</h5>
+							<h6>
+								Population: <span>{item.population}</span>
+							</h6>
+							<h6>
+								Terrain: <span>{item.terrain}</span>
+							</h6>
+							<Button variant="outline-primary">Learn more!</Button>{" "}
+							{store.favorites.includes(item.name) ? null : (
+								<Button onClick={() => actions.setFavorites(item.name)} variant="outline-warning">
+									<BsHeart />
+								</Button>
+							)}
+						</li>
+					);
+				})}
+			</ul>
+		</Jumbotron>
 	);
 };
-
 {
 	/*
 
